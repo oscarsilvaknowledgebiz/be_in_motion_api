946b178472f33d5e48e99f1203a1dd1d4ae0ac69
Daniel Costa <daniel.costa@knowledgebiz.pt>
2023-04-05 16:52:28 +0100

    Task 53 Completed

--- core/schemes/__init__.py
+++ core/schemes/__init__.py
@@ -1,1 +1,2 @@
 from core.schemes import user_schemes
+from core.schemes import medical_prescription_schemes
--- routers/__init__.py
+++ routers/__init__.py
@@ -1,1 +1,2 @@
 from routers import user_router
+from routers import medical_prescription_router
