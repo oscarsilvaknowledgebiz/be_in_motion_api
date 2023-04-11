de96bc54f63b31a7a4a232a2265a5cedab9ae5e5
Daniel Costa <daniel.costa@knowledgebiz.pt>
2023-04-11 11:28:30 +0100

    Task 59 Done

---
+++ routers/test__user_special_condition.py
@@ -0,0 +1,32 @@
+import json
+import internal
+import requests
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+prefix = "user-special-condition"
+
+
+def test_user_special_condition_success():
+    end_point = "by-id"
+    test_id = "ff567cf3-f336-471b-95db-1d1ab9eccfe6"
+    path = f"/{prefix}/{end_point}/{test_id}".format(prefix=prefix, end_point=end_point, test_id=test_id)
+    response = client.get(path)
+    response_json = response.json()
+    assert response.status_code == 200
+    assert response_json["msg"] == "success"
+    assert type(response_json["data"]) == dict
+
+
+def test_user_special_condition_error():
+    end_point = "by-id"
+    test_id = "ff567cf3-f336-471b-95db1d1ab9eccfe6"
+    path = f"/{prefix}/{end_point}/{test_id}".format(prefix=prefix, end_point=end_point, test_id=test_id)
+    response = client.get(path)
+    response_json = response.json()
+    assert response.status_code == 400
+    assert response_json["msg"] == "error"
+    assert response_json["data"] == "This ID is invalid"
